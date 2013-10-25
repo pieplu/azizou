0015e09b4cd412d4178360f6da33251b56e6a992
Pieplu <alexis.pieplu@gmail.com>
2013-10-25 00:46:55 -0400

    création test

---
+++ azizou Tests/azizou_Tests.m
@@ -0,0 +1,34 @@
+//
+//  azizou_Tests.m
+//  azizou Tests
+//
+//  Created by Alexis Piéplu on 2013-10-25.
+//  Copyright (c) 2013 INF3135. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface azizou_Tests : XCTestCase
+
+@end
+
+@implementation azizou_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
