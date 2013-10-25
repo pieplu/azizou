1c05ad05c33fe365a6174e0fd3d7348fc63600d7
Pieplu <alexis.pieplu@gmail.com>
2013-10-25 17:32:18 -0400

    cas5 et 3 fait première partie

--- azizou/filtre.c
+++ azizou/filtre.c
@@ -10,32 +10,45 @@ void errInconnue(){
     signaler_erreur(OPTION_INCONNUE_ERREUR);
     exit(1);
 }
-
 void errOuverture(void){
     signaler_erreur(OUVERTURE_FICHIER_ERREUR);
     exit(1);
 }
 
+
+int checkOpt1(const char **argv){
+    return strcmp(&argv[2][1], "C") || strcmp(&argv[2][1], "L")    ;
+}
+
+int checkOpt2(const char **argv){
+    return strcmp(&argv[4][1], "L") || strcmp(&argv[4][1], "C");
+}
+
+int checkTiretLettre(const char** argv, int position){
+    return strcmp(&argv[position][0], "-");
+}
+
+void cas3(const char** argv){
+    if (checkOpt1(argv) || checkTiretLettre(argv,2)){
+        errInconnue();
+    }
+}
+
 void cas5(const char** argv)
 {
-    //Noms paramètres
-    if (strcmp(&argv[2][1], "C")
-        || strcmp(&argv[2][1], "L")
-        || strcmp(&argv[4][1], "L")
-        || strcmp(&argv[4][1], "C")){
-        signaler_erreur(OPTION_INCONNUE_ERREUR);
-        exit(1);
+    if (checkOpt1(argv) || checkOpt2(argv) || checkTiretLettre(argv,2) || checkTiretLettre(argv,4)){
+        errInconnue();
     }
 }
 
 void nombreParametre(int argc, const char *argv[]){
     // nombre params
     switch (argc){
-        case 1:
+        case 1: //FONCTION errInconnue() à mettre
             break;
         case 2:
             break;
-        case 3:
+        case 3: cas3(argv);
             break;
         case 4:
             break;
@@ -58,7 +71,6 @@ int main(int argc, const char * argv[])
 	
     //Check le nombre de paramètre et dispatche les vérifications
     nombreParametre(argc, argv);
-    
     
     
     
