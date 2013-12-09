a323e8fe3bf9d6f5977b882dcd02916777f22f0e
Pieplu <alexis.pieplu@gmail.com>
2013-12-09 17:46:25 -0500

    javadooooooc!

--- azizou/fonctions.c
+++ azizou/fonctions.c
@@ -10,27 +10,36 @@
 
 
 
-
-// retourne le nombre de lignes total du fichier pointé par fp
+/*
+ * retourne le nombre de lignes total du fichier pointé par fp
+ * Compatible avec les fichier dans '\n' sur leur dernière ligne (sous sublime text, MacOsX...)
+ * Retourne une ligne de plus pour les autres (sous Linux/Unix, Windows ...)
+ *@param fp   : le fichier
+ *@return le nombre de lignes
+ */
 int nbre_lignes_fichier(FILE *fp)
 {
-	int nbLigne=1;
-	char c = 0;
-	if(fp != NULL){
-		while (c != EOF){
-			c = fgetc(fp);
-			if (c == '\n'){
-				nbLigne++;
-			}
-		}
-	}
+    int nbLigne=1;
+    char c = 0;
+    if(fp != NULL){
+        while (c != EOF){
+            c = fgetc(fp);
+            if (c == '\n'){
+                nbLigne++;
+            }
+        }
+    }
     rewind(fp);
-	return nbLigne;
+    return nbLigne;
 }
 
 
-// retourne le max du tableau pointé par vecteur et de taille v
-// retourne 0 si le tableau est vide (NULL)
+/*
+ *retourne la plus haute valeur des case du tableau vecteur
+ *@param vecteur        : le tableau
+ *@param v : taille du tableau vecteur
+ *@return la plus haute valeur ou 0 si le tableau est vide (NULL)
+ */
 int taille_max_lignes(int * vecteur , int v){
     int maxVal=0;
     for(int i=0; i < v; i++){
@@ -41,7 +50,12 @@ int taille_max_lignes(int * vecteur , int v){
 
 
 
-// retourne le nombre de case contenant 0 dans le vecteur
+/*
+ *retourne le nombre de case contenant 0 dans un tableau
+ *@param vecteur        : le tableau
+ *@param taille_vecteur : taille du tableau vecteur
+ *@return le nombre de case contenant 0
+ */
 int NbCaseZero(int *vecteur, int taille_vecteur)
 {
     int ligne0=0;
@@ -54,10 +68,16 @@ int NbCaseZero(int *vecteur, int taille_vecteur)
 }
 
 
-//vecteur[i] représente le nombre d'entiers à ligne i du fichier pointé par fp.
-//Cette fonction retourne un pointeur sur la première case d'un tableau 2D n x m
-//Le tableau 2D de taille n x max_veteur est rempli à partir du fichier.
-//Chaque ligne du fichier qui contient au moins un entier donne lieu à une ligne dans le tableau 2D.
+
+/*
+ *Cette fonction retourne un pointeur sur la première case d'un tableau 2D n x m
+ *Chaque ligne du fichier qui contient au moins un entier donne lieu à une ligne dans le tableau 2D
+ *@param vecteur        : tableau du nombre d'entiers du fichier pointé par fp.
+ *@param fp             : fichier à charger
+ *@param taille_vecteur : taille du tableau vecteur
+ *@param max_vecteur    : plus haute valeur présente dans vecteur, et donc largeur du tableau 2D
+ *@return la taille de la liste
+ */
 char **charger(FILE *fp, int * vecteur, int taille_vecteur, int max_vecteur)
 {
     char curElem[100];
@@ -81,8 +101,11 @@ char **charger(FILE *fp, int * vecteur, int taille_vecteur, int max_vecteur)
 }
 
 
-//verifie si un domaine est syntaxiquement correcte
-// retourne 1 si correcte et 0 sinon
+/*
+ *Verifie si un domaine est syntaxiquement correcte
+ *@param domaine   : le pointeur de chaine de caractère du domaine
+ *@return 1 si correcte et 0 sinon
+ */
 int check_domaine(char *domaine){
     
     int nbTiret=0;
@@ -104,10 +127,13 @@ int check_domaine(char *domaine){
 }
 
 
-//argv représente le tableau des paramères du programme
-//cette fonction retourne la taille de la liste de domaine à partir argv[pos]
-//la fin la liste est localisée soit par la fin du tableau d'argument soit par une option connue
-//pos devrait représenter l'indice de l'option dans argv et retourner la taille de la liste
+
+/*
+ *Cette fonction retourne la taille de la liste de domaines à partir argv[pos]
+ *@param argv   : le tableau des paramères du programme
+ *@param pos    : représenter l'indice de l'option dans argv
+ *@return la taille de la liste
+ */
 int get_nbre_domaines(char  *const argv[], int pos)
 {
     int compt = 0;
@@ -126,9 +152,17 @@ int get_nbre_domaines(char  *const argv[], int pos)
 }
 
 
-//retourne 1 si la syntaxe  de domaine est correcte et place le début et la fin du domaine dans debut et fin
-//retourne 0 si la syntaxe  de domaine est incorrecte
-//max est utilisé pour que <nun>- represente le domaine <num>-max
+
+
+/*
+ *retourne 1 ou 0, en fonction de la syntaxe du domaine soit correcte ou non.
+ *et place le début et la fin du domaine dans debut et fin
+ *@param max        : est utilisé pour que <nun>- represente le domaine <num>-max
+ *@param debut      : début du dommaine à setter
+ *@param fin        : fin du dommaine à setter
+ *@param domaine    : chaine de caractère du domaine
+ *@return 1 pour domaine correct et 0 pour dommaine incorect
+ */
 int get_debut_fin_domaine(char * domaine, int max, int *debut, int *fin)
 {
     unsigned long tailleDomaine = strlen(domaine);
@@ -213,10 +247,13 @@ int get_debut_fin_domaine(char * domaine, int max, int *debut, int *fin)
 }
 
 
-//argv représente le tableau pointant les paramères du programme
-//retourne l'indexe de option dans argv. par exemple esi option est 'C'
-// la valeur retournée est l'indexe de la chaine "-C" dans argv
-// retourne -1 si l'option n'est pas trouvée dans argv
+
+/*
+ *Fusione toutes les matrice présente dans le tableau passer en paramère
+ *@param argv   : tableau pointant les paramères du programme
+ *@param option : option à charcher dans argv
+ *@return l'indexe de option dans argv ou -1 si l'option n'est pas trouvée dans argv
+ */
 int seek_option(char *const argv[], char option)
 {
     int pos = 0;
@@ -341,14 +378,17 @@ void tailleMatriceFinale(int* hauteur, int* largueur, stab2d *tabMatrices, int n
     
 }
 
-//c est soit 'C', soit 'L'.
-//dim est censée représenter soit le nombre de lignes dans le tableau 2D soit le nombre de colonnnes.
-//argv représente le tableau pointant les paramères du programme.
-//retourne un tableau d'entiers de taille dim rempli avec des 0 et des 1 :
-//une case est remplie avec 0 si elle correspond à une ligne ou une colonne qui ne va pas
-//être supprimée selon argv et l'option c ;
-//une case est remplie avec 1 si elle correspond à une ligne ou une colonne qui va
-//être supprimée selon argv et l'option c
+
+
+/*
+ *retourne un tableau d'entiers de taille dim rempli avec des 0 et des 1 :
+ *une case est remplie avec 0 si elle correspond à une ligne ou une colonne qui ne va pas
+ *et avec 1 si elle correspond à une ligne ou une colonne qui va être supprimée
+ *@param c      : soit 'C', soit 'L'.
+ *@param dim    : Taille de la zone à controlé (soit ligne, soit collone du tableau)
+ *@param option : V (fusion Vertical) ou H (fusion Horizontal)
+ *@return l'adresse du tableau setter de 0 ou 1
+ */
 int * control(char *const argv[], int dim, char c){
 
     int *tabRetour=calloc(dim,sizeof(int));
@@ -379,12 +419,18 @@ int * control(char *const argv[], int dim, char c){
     return &(tabRetour[0]);
 }
 
-//mat pointe le début d'un tableau 2D de taill *n x *m.
-//controlC de taille *m est un vecteur indiquant les colonnes à supprimer selon la fonction control.
-//controlV de taille *n est un vecteur indiquant les lignes à supprimer selon la fonction control.
-//retourne un pointeur sur le début d'un tableau 2D de la bonne taille après application
-// des suppressions données par controlC et controlL  et place la nouvelle taille du tableau dans *n et *m.
-//retourne NULL sil le tableau résultant est vide.
+
+
+/*
+ *retourne un pointeur sur le début d'un tableau 2D de la bonne taille après application
+ *des suppressions données par controlC et controlL  et place la nouvelle taille du tableau dans *n et *m.
+ *@param m          : taille de controlC
+ *@param n          : taille de controlL
+ *@param controlC   : vecteur indiquant les colonnes à supprimer selon la fonction control.
+ *@param controlL   : vecteur indiquant les lignes à supprimer selon la fonction control.
+ *@param mat        : pointe le début d'un tableau 2D de taill *n x *m.
+ *@return le pointeur ou NULL si le tableau résultant est vide.
+ */
 char** filter(char** mat, int *n, int *m, int *controlC, int *controlL)
 {
     char* (*tabAvantFiltre)[*m] = NULL;
--- azizou/fonctions.h
+++ azizou/fonctions.h
@@ -5,46 +5,79 @@
 #define FONCTIONS_H
 #define true 1
 #define false 0
+
 typedef struct tab2D stab2d;
 
-// cette fonction est fournie
-//affiche un message d'erreur sur le canal d'erreur selon les cas définis
-void signaler_erreur(int err); // fait
 
+/*
+ * retourne le nombre de lignes total du fichier pointé par fp
+ * Compatible avec les fichier dans '\n' sur leur dernière ligne (sous sublime text, MacOsX...)
+ * Retourne une ligne de plus pour les autres (sous Linux/Unix, Windows ...)
+ *@param fp   : le fichier
+ *@return le nombre de lignes
+ */
+int nbre_lignes_fichier(FILE *fp);
 
-// retourne le nombre de lignes total du fichier pointé par fp
-int nbre_lignes_fichier(FILE *fp); //fait
-
-// retourne le max du tableau pointé par vecteur et de taille v
-// retourne 0 si le tableau est vide (NULL)
+/*
+ *retourne la plus haute valeur des case du tableau vecteur
+ *@param vecteur        : le tableau
+ *@param v : taille du tableau vecteur
+ *@return la plus haute valeur ou 0 si le tableau est vide (NULL)
+ */
 int taille_max_lignes(int * vecteur , int v); //fait
 
-//vecteur[i] représente le nombre d'entiers à ligne i du fichier pointé par fp.
-//si vecteur[i]==0, la ligne i du fichier est ignorée car elle est vide d'entier.
-//Cette fonction retourne un pointeur sur la première case d'un tableau 2D n x m
-//Le tableau 2D de taille n x max_veteur est rempli à partir du fichier.
-//Chaque ligne du fichier qui contient au moins un entier donne lieu à une ligne dans le tableau 2D.
+/*
+ *retourne le nombre de case contenant 0 dans un tableau
+ *@param vecteur        : le tableau
+ *@param taille_vecteur : taille du tableau vecteur
+ *@return le nombre de case contenant 0
+ */
+int NbCaseZero(int *vecteur, int taille_vecteur);
+
+/*
+ *Cette fonction retourne un pointeur sur la première case d'un tableau 2D n x m
+ *Chaque ligne du fichier qui contient au moins un entier donne lieu à une ligne dans le tableau 2D
+ *@param vecteur        : tableau du nombre d'entiers du fichier pointé par fp.
+ *@param fp             : fichier à charger
+ *@param taille_vecteur : taille du tableau vecteur
+ *@param max_vecteur    : plus haute valeur présente dans vecteur, et donc largeur du tableau 2D
+ *@return la taille de la liste
+ */
 char **charger(FILE *fp, int * vecteur, int taille_vecteur, int max_vecteur);
 
-//verifie si un domaine est syntaxiquement correcte
-// retourne 1 si correcte et 0 sinon
+/*
+ *Verifie si un domaine est syntaxiquement correcte
+ *@param domaine   : le pointeur de chaine de caractère du domaine
+ *@return 1 si correcte et 0 sinon
+ */
 int check_domaine(char *domaine) ;
 
-//argv représente le tableau des paramères du programme
-//cette fonction retourne la taille de la liste de domaine à partir argv[pos]
-//la fin la liste est localisée soit par la fin du tableau d'argument soit par une option connue
-//pos devrait représenter l'indice de l'option dans argv et retourner la taille de la liste
+
+/*
+ *Cette fonction retourne la taille de la liste de domaines à partir argv[pos]
+ *@param argv   : le tableau des paramères du programme
+ *@param pos    : représenter l'indice de l'option dans argv
+ *@return la taille de la liste
+ */
 int get_nbre_domaines(char  *const argv[], int pos);
 
-//retourne 1 si la syntaxe  de domaine est correcte et place le début et la fin du domaine dans debut et fin
-//retourne 0 si la syntaxe  de domaine est incorrecte
-//max est utilisé pour que <nun>- represente le domaine <num>-max
+/*
+ *retourne 1 ou 0, en fonction de la syntaxe du domaine soit correcte ou non.
+ *et place le début et la fin du domaine dans debut et fin
+ *@param max        : est utilisé pour que <nun>- represente le domaine <num>-max
+ *@param debut      : début du dommaine à setter
+ *@param fin        : fin du dommaine à setter
+ *@param domaine    : chaine de caractère du domaine
+ *@return 1 pour domaine correct et 0 pour dommaine incorect
+ */
 int get_debut_fin_domaine(char * domaine, int max, int *debut, int *fin);
 
-//argv représente le tableau pointant les paramères du programme
-//retourne l'indexe de option dans argv. par exemple esi option est 'C'
-// la valeur retournée est l'indexe de la chaine "-C" dans argv
-// retourne -1 si l'option n'est pas trouvée dans argv
+/*
+ *Fusione toutes les matrice présente dans le tableau passer en paramère
+ *@param argv   : tableau pointant les paramères du programme
+ *@param option : option à charcher dans argv
+ *@return l'indexe de option dans argv ou -1 si l'option n'est pas trouvée dans argv
+ */
 int seek_option(char *const argv[], char option);
 
 /*
@@ -64,26 +97,30 @@ stab2d fusionMatrices(stab2d *tabMatrices, int nbMatrices, char option);
  *@param option      : V (fusion Vertical) ou H (fusion Horizontal)
  *@return une nouvelle structure contant la fusion des matrices
  */
-
-
 void tailleMatriceFinale(int* hauteur, int* largueur, stab2d *tabMatrices, int nbMatrices, char option);
 
-//c est soit 'C', soit 'L'.
-//dim est censée représenter soit le nombre de lignes dans le tableau 2D soit le nombre de colonnnes.
-//argv représente le tableau pointant les paramères du programme.
-//retourne un tableau d'entiers de taille dim rempli avec des 0 et des 1 :
-//une case est remplie avec 0 si elle correspond à une ligne ou une colonne qui ne va pas
-//être supprimée selon argv et l'option c ;
-//une case est remplie avec 1 si elle correspond à une ligne ou une colonne qui va
-//être supprimée selon argv et l'option c
+/*
+ *retourne un tableau d'entiers de taille dim rempli avec des 0 et des 1 :
+ *une case est remplie avec 0 si elle correspond à une ligne ou une colonne qui ne va pas
+ *et avec 1 si elle correspond à une ligne ou une colonne qui va être supprimée
+ *@param c      : soit 'C', soit 'L'.
+ *@param dim    : Taille de la zone à controlé (soit ligne, soit collone du tableau)
+ *@param option : V (fusion Vertical) ou H (fusion Horizontal)
+ *@return l'adresse du tableau setter de 0 ou 1
+ */
 int * control(char *const argv[], int dim, char c);
 
-//mat pointe le début d'un tableau 2D de taill *n x *m.
-//controlC de taille *m est un vecteur indiquant les colonnes à supprimer selon la fonction control.
-//controlV de taille *n est un vecteur indiquant les lignes à supprimer selon la fonction control.
-//retourne un pointeur sur le début d'un tableau 2D de la bonne taille après application
-// des suppressions données par controlC et controlL  et place la nouvelle taille du tableau dans *n et *m.
-//retourne NULL sil le tableau résultant est vide.
+
+/*
+ *retourne un pointeur sur le début d'un tableau 2D de la bonne taille après application
+ *des suppressions données par controlC et controlL  et place la nouvelle taille du tableau dans *n et *m.
+ *@param m          : taille de controlC
+ *@param n          : taille de controlL
+ *@param controlC   : vecteur indiquant les colonnes à supprimer selon la fonction control.
+ *@param controlL   : vecteur indiquant les lignes à supprimer selon la fonction control.
+ *@param mat        : pointe le début d'un tableau 2D de taill *n x *m.
+ *@return le pointeur ou NULL si le tableau résultant est vide.
+ */
 char** filter(char** mat, int *n, int *m, int *controlC, int *controlL);
 
 
